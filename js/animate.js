20a8775263119e0ab6aec499716eb7a4606e84bb
aaronjospeh <aaronnomor2@gmail.com>
2024-11-07 13:08:00 +0700

    Add highligh function in animate.js

--- js/animate.js
+++ js/animate.js
@@ -1,16 +1,39 @@
-$(document).ready(function() {
-    $('.king').hover(
-        function() {
-            $(this).animate({
-                marginTop: "-10px",
-                boxShadow: "0px 10px 20px rgba(0, 0, 0, 0.2)"
-            }, 300);
+//Hover animation - 1302220001
+$(document).ready(function () {
+  $(".king").hover(
+    function () {
+      $(this).animate(
+        {
+          marginTop: "-10px",
+          boxShadow: "0px 10px 20px rgba(0, 0, 0, 0.2)",
         },
-        function() {
-            $(this).animate({
-                marginTop: "0px",
-                boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)"
-            }, 300);
-        }
-    );
+        300
+      );
+    },
+    function () {
+      $(this).animate(
+        {
+          marginTop: "0px",
+          boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+        },
+        300
+      );
+    }
+  );
 });
+
+// Highlight animation - 1302223083
+$(".highlight").hover(
+  //hover state
+  function () {
+    $(this).css({
+      "background-color": "#ffff77",
+    });
+  },
+  //default state
+  function () {
+    $(this).css({
+      "background-color": "",
+    });
+  }
+);
