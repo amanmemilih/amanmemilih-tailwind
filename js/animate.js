39222cd0ece1f4aa20d25c64dc56faa24a2fc56f
wildanniam <wildanniam4@gmail.com>
2024-11-08 06:10:24 +0700

    add Jquery for button wildan

--- js/animate.js
+++ js/animate.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('.king').hover(
-        function() {
+        function () {
             $(this).animate({
                 marginTop: "-10px",
                 boxShadow: "0px 10px 20px rgba(0, 0, 0, 0.2)"
             }, 300);
         },
-        function() {
+        function () {
             $(this).animate({
                 marginTop: "0px",
                 boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)"
@@ -14,3 +14,25 @@ $(document).ready(function() {
         }
     );
 });
+
+
+// JQUERY Wildan
+$(document).ready(function () {
+    $(".confettiButton").on("click", function () {
+        confettiAnimation(this);
+    });
+});
+
+function confettiAnimation(element) {
+    // Dapatkan posisi tombol di layar
+    const rect = element.getBoundingClientRect();
+    const originX = (rect.left + rect.width / 2) / window.innerWidth;
+    const originY = (rect.top + rect.height / 2) / window.innerHeight;
+
+    // Luncurkan confetti dari posisi tombol
+    confetti({
+        particleCount: 100,
+        spread: 100,
+        origin: { x: originX, y: originY }
+    });
+}
